ddc00bf624d455172ac6d38c55320c08b881b622
PullakanamRamyaLakshmiDurga <72567587+PullakanamRamyaLakshmiDurga@users.noreply.github.com>
2023-02-27 15:55:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        //<p>
-          //Edit <code>src/App.js</code> and save to reload.
-        //</p>
+       
          <h1> HELLO DURGA </h1>
         <a
           className="App-link"
